c4bd21bbceef61382325ffd921834b9bcf7ff676
Aaron Sanchez <111822002+Svndda@users.noreply.github.com>
2026-05-04 15:53:27 -0600

    fix: deletion of unnecessary commands.

--- api/composer-scripts.bash
+++ api/composer-scripts.bash
@@ -1,9 +1,5 @@
 # Installation.
 composer install
 
-# Install Symfony UID component
-composer require symfony/uid
-
 # Update dependencies
 composer update
-
--- api/stan-scripts.bash
+++ api/stan-scripts.bash
@@ -1,6 +1,2 @@
-
-# Install PHPStan
-composer require --dev phpstan/phpstan
-
 # Run PHPStan analysis
 vendor/bin/phpstan analyse src --level=max
