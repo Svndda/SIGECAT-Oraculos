42dee7bcbae7b462ac7fd7473eb6b03a8ce791d2
henry24rf <henryrojasf24@gmail.com>
2026-06-29 19:47:07 -0600

    fix(JobClassDTO): File name correction and validation in CreateJobClassDTO.

--- api/src/DTO/CreateJobClassDTO.php
+++ api/src/DTO/CreateJobClassDTO.php
@@ -54,7 +54,7 @@ final class CreateJobClassDTO {
   }
 
   public function validate(): void {
-    if (empty($this->jobClassCode)) {
+    if ($this->jobClassCode > 0) {
       throw new ApiException(ErrorType::missingField('job_class_code'));
     }
 
